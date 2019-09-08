f05abb7fdb78c5575f438cc0260a9649c874e8f1
FirstWisdom <jason.zelaya21@yahoo.com>
2019-09-08 09:28:27 -0700

    add event listeners and code to play sounds in index.js

---
+++ assets/js/index.js
@@ -0,0 +1,75 @@
+// --------------EVENT LISTENERS---------------------
+
+// Trigger event listeners on click
+$('.drum').click(function() {
+  // The key pressed
+  var buttonClicked = this.html();
+
+  // Play a sound for each button
+  playSound(buttonClicked);
+
+});
+
+
+// Trigger event listeners on keypress
+$(document).keypress(function(evt) {
+
+  // Play a sound for each button
+  playSound(evt.key);
+
+});
+
+
+// --------------FUNCTIONS---------------------
+
+
+// Play a sound contingent upon the relative button/key event fired
+function playSound(key) {
+
+  switch (key) {
+    // Play tom-1.mp3 sound if the "w" button event is fired
+    case "w":
+      var tom1 = new Audio("assets/sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    // Play tom-2.mp3 sound if the "a" button event is fired
+    case "a":
+      var tom2 = new Audio("assets/sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    // Play tom-3.mp3 sound if the "s" button event is fired
+    case "s":
+      var tom3 = new Audio('assets/sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    // Play tom-4.mp3 sound if the "d" button event is fired
+    case "d":
+      var tom4 = new Audio('assets/sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    // Play snare.mp3 sound if the "j" button event is fired
+    case "j":
+      var snare = new Audio('assets/sounds/snare.mp3');
+      snare.play();
+      break;
+
+    // Play crash.mp3 sound if the "k" button event is fired
+    case "k":
+      var crash = new Audio('assets/sounds/crash.mp3');
+      crash.play();
+      break;
+
+    // // Play kick-bass.mp3 sound if the "w" button event is fired
+    case "l":
+      var kick = new Audio('assets/sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+    // If none of the above events are fired print the pressed key to the console
+    default: console.log(key);
+    }
+  }
